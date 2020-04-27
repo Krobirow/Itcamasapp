7e8d7678ae5a8d2ef799b58b8f1781e605400dc4
cMeTaHKo <bodia122@gmail.com>
2020-04-27 16:15:35 +0200

    lesson 21 upd

--- src/Components/Dialogs/Dialogs.js
+++ src/Components/Dialogs/Dialogs.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import s from './dialogs.module.css';
+import React from "react";
+import s from "./dialogs.module.css";
+import { NavLink } from "react-router-dom";
 
 const Dialogs = (props) => {
 	return (
 		<div className={s.dialogsWrap}>
-			dialogs
+			<div className={s.dialogsItem}>
+				<div className={s.dialog}>
+					<NavLink className={s.dialogLink + ' ' + s.active} activeClassName={s.active} to="#$">Andrey</NavLink>
+				</div>
+				<div className={s.dialog}>
+					<NavLink className={s.dialogLink} activeClassName={s.active} to="#%">Eugenij</NavLink>
+				</div>
+				<div className={s.dialog}>
+					<NavLink className={s.dialogLink} activeClassName={s.active} to="#^">Sasha</NavLink>
+				</div>
+				<div className={s.dialog}>
+					<NavLink className={s.dialogLink} activeClassName={s.active} to="/Ania">Ania</NavLink>
+				</div>
+				<div className={s.dialog}>
+					<NavLink className={s.dialogLink} activeClassName={s.active} to="/Vasya">Vasya</NavLink>
+				</div>
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}> How are You ?</div>
+				<div className={s.message}> How are they?</div>
+				<div className={s.message}> How could You ?</div>
+				<div className={s.message}> How much You can ?</div>
+				<div className={s.message}> How long is it ?</div>
+			</div>
 		</div>
-	)
-}
+	);
+};
 
 export default Dialogs;
