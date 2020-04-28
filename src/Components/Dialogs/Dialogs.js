cf8feb0fb2117da97f6fdbee1be1a6bb942d6dc9
cMeTaHKo <bodia122@gmail.com>
2020-04-28 16:32:50 +0200

    lesson 25 upd

--- src/Components/Dialogs/Dialogs.js
+++ src/Components/Dialogs/Dialogs.js
@@ -6,37 +6,37 @@ import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 	let dialogsData = [
-		{id: 1, name: 'Dimych'},
-		{id: 2, name: 'Andrey'},
-		{id: 3, name: 'Eugenij'},
-		{id: 4, name: 'Sasha'},
-		{id: 5, name: 'Ania'},
-		{id: 6, name: 'Vasya'},
-		{id: 7, name: 'Maks'},
-	]
+		{ id: 1, name: "Dimych" },
+		{ id: 2, name: "Andrey" },
+		{ id: 3, name: "Eugenij" },
+		{ id: 4, name: "Sasha" },
+		{ id: 5, name: "Ania" },
+		{ id: 6, name: "Vasya" },
+		{ id: 7, name: "Maks" },
+	];
 
 	let messagesData = [
-		{id: 1, message: 'How do You feel?'},
-		{id: 2, message: 'How are You ?'},
-		{id: 3, message: 'How are they?'},
-		{id: 4, message: 'How could You ?'},
-		{id: 5, message: 'How much You can ?'},
-		{id: 6, message: 'How long is it ?'},
-		{id: 7, message: 'Oh, Hi Mark!'},
-	]
+		{ id: 1, message: "How do You feel?" },
+		{ id: 2, message: "How are You ?" },
+		{ id: 3, message: "How are they?" },
+		{ id: 4, message: "How could You ?" },
+		{ id: 5, message: "How much You can ?" },
+		{ id: 6, message: "How long is it ?" },
+		{ id: 7, message: "Oh, Hi Mark!" },
+	];
+
+	let dialogElements = dialogsData.map( d => <DialogItem name={d.name} id={d.id} />);
+
+	let messageElements = messagesData.map( m => <Message message={m.message} id={m.id} />);
 
 	return (
 		<div className={s.dialogsWrap}>
 			<div className={s.dialogsItem}>
-				<DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-				<DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-				<DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+				{dialogElements}
 			</div>
-			
+
 			<div className={s.messages}>
-				<Message message={messagesData[0].message} id={messagesData[0].id} />
-				<Message message={messagesData[1].message} id={messagesData[1].id} />
-				<Message message={messagesData[2].message} id={messagesData[2].id} />
+				{messageElements}
 			</div>
 		</div>
 	);
--- src/Components/Profile/MyPosts/MyPosts.js
+++ src/Components/Profile/MyPosts/MyPosts.js
@@ -6,33 +6,30 @@ import Post from './Post/Post';
 const MyPosts = () => {
 
 	let myPostData = [
-		{id: 1, message: "It's my first post", likesCount: 34, dislikesCount: 5},
-		{id: 2, message: "Hi, how are you ?", likesCount: 3, dislikesCount: 0},
+		{ id: 1, message: "It's my first post", likesCount: 34, dislikesCount: 5 },
+		{ id: 2, message: "Hi, how are you ?", likesCount: 3, dislikesCount: 0 },
 	]
+	let myPostsEl = myPostData
+		.map(p => <Post id={p.id}
+			message={p.message}
+			likesCount={p.likesCount}
+			dislikesCount={p.dislikesCount}
+		/>);
 
 	return (
-	<div className={s.myPostsWrap}>
-		<div>
-			<h2>My Posts</h2>
+		<div className={s.myPostsWrap}>
+			<div>
+				<h2>My Posts</h2>
+			</div>
+			<div>
+				<textarea name="newPost" cols="20" rows="5"></textarea>
+			</div>
+			<div>
+				<button type="button">Add post</button>
+			</div>
+			<h3 className={s.myPostsTitle}> New Posts</h3>
+			{myPostsEl}
 		</div>
-		<div>
-			<textarea name="newPost" cols="20" rows="5"></textarea>
-		</div>
-		<div>
-			<button type="button">Add post</button>
-		</div>
-		<h3 className={s.myPostsTitle}> New Posts</h3>
-		<Post id={myPostData[0].id} 
-			message={myPostData[0].message} 
-			likesCount={myPostData[0].likesCount} 
-			dislikesCount={myPostData[0].dislikesCount}
-		/>
-		<Post id={myPostData[1].id} 
-			message={myPostData[1].message} 
-			likesCount={myPostData[1].likesCount} 
-			dislikesCount={myPostData[1].dislikesCount}
-		/>
-	</div>
 	);
 }
 
