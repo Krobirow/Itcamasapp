f483f5f6a1319f00ea89bf8fdb1b2cb68c2c2544
cMeTaHKo <bodia122@gmail.com>
2020-04-28 16:05:31 +0200

    lesson 24 upd

--- src/Components/Dialogs/Dialogs.js
+++ src/Components/Dialogs/Dialogs.js
@@ -5,26 +5,38 @@ import Message from "./Message/Message";
 // import { NavLink } from "react-router-dom";
 
 const Dialogs = (props) => {
+	let dialogsData = [
+		{id: 1, name: 'Dimych'},
+		{id: 2, name: 'Andrey'},
+		{id: 3, name: 'Eugenij'},
+		{id: 4, name: 'Sasha'},
+		{id: 5, name: 'Ania'},
+		{id: 6, name: 'Vasya'},
+		{id: 7, name: 'Maks'},
+	]
+
+	let messagesData = [
+		{id: 1, message: 'How do You feel?'},
+		{id: 2, message: 'How are You ?'},
+		{id: 3, message: 'How are they?'},
+		{id: 4, message: 'How could You ?'},
+		{id: 5, message: 'How much You can ?'},
+		{id: 6, message: 'How long is it ?'},
+		{id: 7, message: 'Oh, Hi Mark!'},
+	]
+
 	return (
 		<div className={s.dialogsWrap}>
 			<div className={s.dialogsItem}>
-				<DialogItem name="Andrey" id="1" />
-				<DialogItem name="Eugenij" id="2" />
-				<DialogItem name="Sasha" id="3" />
-				<DialogItem name="Ania" id="4" />
-				<DialogItem name="Vasya" id="5" />
-				<DialogItem name="Igor" id="6" />
-				<DialogItem name="Maks" id="7" />
+				<DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+				<DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+				<DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
 			</div>
 			
 			<div className={s.messages}>
-				<Message message="How do You feel?" />
-				<Message message="How are You ?" />
-				<Message message="How are they?" />
-				<Message message="How could You ?" />
-				<Message message="How much You can ?" />
-				<Message message="How long is it ?" />
-				<Message message="Oh, Hi Mark!" />
+				<Message message={messagesData[0].message} id={messagesData[0].id} />
+				<Message message={messagesData[1].message} id={messagesData[1].id} />
+				<Message message={messagesData[2].message} id={messagesData[2].id} />
 			</div>
 		</div>
 	);
--- src/Components/Profile/MyPosts/MyPosts.js
+++ src/Components/Profile/MyPosts/MyPosts.js
@@ -4,6 +4,12 @@ import s from './myposts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+	let myPostData = [
+		{id: 1, message: "It's my first post", likesCount: 34, dislikesCount: 5},
+		{id: 2, message: "Hi, how are you ?", likesCount: 3, dislikesCount: 0},
+	]
+
 	return (
 	<div className={s.myPostsWrap}>
 		<div>
@@ -16,8 +22,16 @@ const MyPosts = () => {
 			<button type="button">Add post</button>
 		</div>
 		<h3 className={s.myPostsTitle}> New Posts</h3>
-		<Post message="It's my first post" likesCount="34" dislikesCount="5"/>
-		<Post message="Hi, how are you ?" likesCount="0" dislikesCount="3"/>
+		<Post id={myPostData[0].id} 
+			message={myPostData[0].message} 
+			likesCount={myPostData[0].likesCount} 
+			dislikesCount={myPostData[0].dislikesCount}
+		/>
+		<Post id={myPostData[1].id} 
+			message={myPostData[1].message} 
+			likesCount={myPostData[1].likesCount} 
+			dislikesCount={myPostData[1].dislikesCount}
+		/>
 	</div>
 	);
 }
