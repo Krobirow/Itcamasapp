2cded2ad6f76323f9a73349bcc5ed2f8bd3caf4d
Krobirow <krobirow@gmail.com>
2020-05-30 10:43:59 +0200

    Lesson 85 UPD

--- src/Components/Profile/ProfileInfo/ProfileInfo.js
+++ src/Components/Profile/ProfileInfo/ProfileInfo.js
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './profileInfo.module.css';
 import Preloader from '../../common/Preloader/Preloader';
 import ProfileStatus from './ProfileStatus'
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 
 const ProfileInfo = props => {
@@ -11,7 +12,7 @@ const ProfileInfo = props => {
 			<div className={s.avatarDescr}>
 				<img src={props.profile.photos.large ? props.profile.photos.large : 'https://wallpapercave.com/wp/PCG5mFl.jpg'} alt="ava" />
 				<div>
-					<ProfileStatus status={props.status} updateUserStatus={props.updateUserStatus}/>
+					<ProfileStatusWithHooks status={props.status} updateUserStatus={props.updateUserStatus}/>
 				</div>
 				
 			</div>
