59ff5919a0ee031a365eab1d66bb65d5716825d2
Krobirow <krobirow@gmail.com>
2020-06-01 13:26:32 +0200

    lesson 88 UPD

--- src/Components/Profile/MyPosts/MyPosts.js
+++ src/Components/Profile/MyPosts/MyPosts.js
@@ -1,57 +1,65 @@
-import React from 'react';
-import s from './myposts.module.css';
+import React, { PureComponent } from "react";
+import s from "./myposts.module.css";
 
-import Post from './Post/Post';
-import { Field, reduxForm } from 'redux-form';
-import { required, maxlengthCreator } from '../../../utils/validators/validators';
-import { Textarea } from '../../common/FormsControls/FormsControls';
+import Post from "./Post/Post";
+import { Field, reduxForm } from "redux-form";
+import {
+	required,
+	maxlengthCreator,
+} from "../../../utils/validators/validators";
+import { Textarea } from "../../common/FormsControls/FormsControls";
 
 const maxLength = maxlengthCreator(10);
 
-const MyPostsForm = props => {
-	return(
+const MyPostsForm = (props) => {
+	return (
 		<form onSubmit={props.handleSubmit} className={s.form}>
 			<div>
-				<Field component={Textarea}
-				name="newPostText" 
-				placeholder="Enter Your post"
-				validate={[required, maxLength]} />
+				<Field
+					component={Textarea}
+					name='newPostText'
+					placeholder='Enter Your post'
+					validate={[required, maxLength]}
+				/>
 			</div>
 			<div>
-				<button type="submit">Add post</button>
+				<button type='submit'>Add post</button>
 			</div>
 		</form>
-	)
-}
+	);
+};
 const MyPostsReduxForm = reduxForm({ form: "addPost" })(MyPostsForm);
 
-const MyPosts = (props) => {
-	let {myPostData} = props;
+const MyPosts = React.memo( props => {
 
-	let myPostsEl = myPostData
-		.map(p => <Post key={p.id}
-			id={p.id}
-			postText={p.postText}
-			likesCount={p.likesCount}
-			dislikesCount={p.dislikesCount}
-		/>);
+		console.log('render myPosts');
 
-	const addNewPost = (values) => {
-		props.addPost(values.newPostText);
-	}
+		let myPostsEl = props.myPostData.map((p) => (
+			<Post
+				key={p.id}
+				id={p.id}
+				postText={p.postText}
+				likesCount={p.likesCount}
+				dislikesCount={p.dislikesCount}
+			/>
+		));
 
-	return (
-		<div className={s.myPostsWrap}>
-			<div>
-				<h2>My Posts</h2>
+		const addNewPost = (values) => {
+			props.addPost(values.newPostText);
+		};
+
+		return (
+			<div className={s.myPostsWrap}>
+				<div>
+					<h2>My Posts</h2>
+				</div>
+				<div className={s.MyPostsFormWraper}>
+					<MyPostsReduxForm onSubmit={addNewPost} />
+				</div>
+				<h3 className={s.myPostsTitle}> New Posts</h3>
+				{myPostsEl}
 			</div>
-			<div className={s.MyPostsFormWraper}>
-				<MyPostsReduxForm onSubmit={addNewPost}/>
-			</div>
-			<h3 className={s.myPostsTitle}> New Posts</h3>
-			{myPostsEl}
-		</div>
-	);
-}
+		);
+})
 
 export default MyPosts;
