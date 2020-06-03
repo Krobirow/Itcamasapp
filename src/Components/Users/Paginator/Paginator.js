7f18385ac7a52a4710d2d8e84ebd9232b86e805f
Krobirow <krobirow@gmail.com>
2020-06-03 18:53:41 +0200

    lesson 93 with fix needed

--- src/Components/Users/Paginator/Paginator.js
+++ src/Components/Users/Paginator/Paginator.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import s from './paginator.module.css';
+import React, { useState } from "react";
+import s from "./paginator.module.css";
 
-
-const Paginator = ({currentPage, totalUsersCount, pageSize, onPageChanger, ...props}) => {
+const Paginator = ({
+	currentPage,
+	totalUsersCount,
+	pageSize,
+	onPageChanger,
+	portionSize = 455,
+}) => {
 	let pagesCount = Math.ceil(totalUsersCount / pageSize);
 
 	let pages = [];
@@ -10,26 +15,49 @@ const Paginator = ({currentPage, totalUsersCount, pageSize, onPageChanger, ...pr
 		pages.push(i);
 	}
 
+	let portionCount = Math.ceil(pagesCount / portionSize);
+	let [portionNumber, setPortionNumber] = useState(1);
+	let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+	let rightPortionPageNumber = portionNumber * portionSize;
+
 	return (
-        <div className={s.paginationWrapp}>
-            {pages.map((page) => {
-                {/* if (page <= 25) { */}
-                    return (
-                        <span
-                            key={page}
-                            onClick={(e) => {
-                                onPageChanger(page);
-                            }}
-                            className={
-                                currentPage === page ? s.selectedPage : undefined
-                            }>
-                            {page}
-                        </span>
-                    );
-                {/* } */}
-            })}
-        </div>
-        )
-}
+		<div className={s.paginationWrapp}>
+			{portionNumber ? (
+				<button disabled={portionNumber === 1}
+					onClick={() => {
+						setPortionNumber(portionNumber - 1);
+					}}>
+					PREV
+				</button>
+			) : portionNumber > 1 }
+
+			{pages
+				.filter(page =>
+						page >= leftPortionPageNumber && page <= rightPortionPageNumber
+				)
+				.map(page => {
+					return (
+						<span
+							key={page}
+							onClick={(e) => {
+								onPageChanger(page);
+							}}
+							className={currentPage === page ? s.selectedPage : undefined}>
+							{page}
+						</span>
+					);
+				})}
+			{
+				<button
+                    disabled={portionCount < portionNumber}
+					onClick={() => {
+						setPortionNumber(portionNumber + 1);
+					}}>
+					NEXT
+				</button>
+			}
+		</div>
+	);
+};
 
 export default Paginator;
