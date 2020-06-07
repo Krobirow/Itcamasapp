ea419b3732fca19182dfa9f9d2b5284c0ed31062
Krobirow <krobirow@gmail.com>
2020-06-07 12:27:09 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactJS-Social-Network/precache-manifest.c7278d2e65c12827d6f79e6230112a20.js"
+  "/ReactJS-Social-Network/precache-manifest.bcc40a5d3b2f40ef3f4e4d8aac1c5bd2.js"
 );
 
 self.addEventListener('message', (event) => {
