cef0bd313d36a34f21fb6b4b94186367e428753b
Krobirow <krobirow@gmail.com>
2020-06-12 22:30:19 +0200

    wiki request test

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import s from './footer.module.css';
+import * as axios from 'axios';
 
 const Footer = () => {
+
+	const instanceWiki = axios.create({
+		withCredentials: true,
+		baseURL: 'https://en.wikipedia.org/w/api.php?',
+		crossdomain: true,
+		credentials: 'same-origin',
+		mode: 'same-origin',
+		headers: {
+			'Access-Control-Allow-Origin' : '*',
+			'Content-Type': 'text/html'
+		}
+	})
+
+	function getWiki() {
+		return instanceWiki.get(`action=parse&page=Pet_door&prop=text&formatversion=2`)
+		.then(response => { console.log(response);})
+	}
+
+	getWiki();
+
 	return (
 		<footer className={s.footer}>
 			<div >
